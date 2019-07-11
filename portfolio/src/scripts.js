5d2f47d5ce3755b0b57abc5824f67c8db9350125
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2019-07-11 11:51:25 +0200

    Update scripts.js

--- portfolio/src/scripts.js
+++ portfolio/src/scripts.js
@@ -1,9 +1,18 @@
+// <!-- jQuery Slick --> 
+  $(document).ready(() => {
 
-// Script tags 
-<script type="text/javascript" src="app.js"/>
-<script src="https://unpkg.com/aos@next/dist/aos.js"/>
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"/>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"/>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"/>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"/>
+    $(window).scroll( () => {
+      let scroll = $(window).scrollTop();
+      let offset = $('.cover').offset().top; 
+      let height = $('.cover').height(); 
+      if (scroll > offset + height) {
+        $('.nav-link').css("color","black");
+        $('.navbar-brand').css("color","black");
+        $('.nav-link:hover').addClass("border-top");
+      } else {
+        $('.nav-link').css("color","white");
+        $('.navbar-brand').css("color","white");
+        $('.nav-link').removeClass("border-top");
+      }
+    });
+  });
