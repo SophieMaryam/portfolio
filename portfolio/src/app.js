c4a06eb96b128800d3a0c6a3eaed0809564b1f76
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2019-07-17 10:07:38 +0200

    Update src path on app.js

--- portfolio/src/app.js
+++ portfolio/src/app.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const app   = express();
 const particles = require('particles.js');
-
-
 const port  = process.env.PORT || 4000;
 
 // Set public folder as root
-app.use(express.static("src"));
+app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static("../public/images"));
 
 // Allow front-end access to node_modules folder
-app.use("/scripts", express.static(`${__dirname}/node_modules/`));
+// app.use("app.js", express.static(`${__dirname}/node_modules/`));
 
 
 // Listen for HTTP requests on port 3000
