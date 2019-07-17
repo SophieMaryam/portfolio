d9a19e44e364d718c7812db7b9e9ad0d11d74b88
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2019-07-17 13:32:09 +0200

    Remove unused code

--- portfolio/src/app.js
+++ portfolio/src/app.js
@@ -1,20 +1,25 @@
 const express = require("express");
 const app   = express();
-const particles = require('particles.js');
 const port  = process.env.PORT || 4000;
 
 // Set public folder as root
-app.use(express.static(path.join(__dirname, "public")));
 // app.use(express.static("../public/images"));
 
 // Allow front-end access to node_modules folder
 // app.use("app.js", express.static(`${__dirname}/node_modules/`));
 
-
+app.get('/', function(req, res, next) {
+  res.render('index');
+});
 // Listen for HTTP requests on port 3000
 app.listen(port, () => {
   console.log("listening on %d", port);
 });
 
+// window.onload = function() {
+//   Particles.init({
+//     selector: '.background'
+//   });
+// };
 
 module.exports = app;
