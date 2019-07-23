3cf3a60265ec6e284531a38e062255e1c236c8f0
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2019-07-23 13:37:43 +0200

    Fix http get request

--- portfolio/src/app.js
+++ portfolio/src/app.js
@@ -1,25 +1,21 @@
 const express = require("express");
 const app   = express();
+const path = require('path');
 const port  = process.env.PORT || 4000;
 
-// Set public folder as root
-// app.use(express.static("../public/images"));
+var bodyParser = require('body-parser');
+app.use('/', bodyParser.urlencoded({extended:true}));
 
-// Allow front-end access to node_modules folder
-// app.use("app.js", express.static(`${__dirname}/node_modules/`));
+// Set src folder as root
+app.use(express.static(__dirname + "/../public"));
 
-app.get('/', function(req, res, next) {
-  res.render('index');
+app.get("/", (req, res, next) => {
+  res.sendFile("src/views/index.html");
 });
+
 // Listen for HTTP requests on port 3000
 app.listen(port, () => {
   console.log("listening on %d", port);
 });
-
-// window.onload = function() {
-//   Particles.init({
-//     selector: '.background'
-//   });
-// };
 
 module.exports = app;
