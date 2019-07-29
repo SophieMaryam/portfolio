0173b51c74429c2c0b2ad15da7fbd0719b989b8e
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2019-07-29 09:44:17 +0200

    Update main.js

--- portfolio/public/javascript/main.js
+++ portfolio/public/javascript/main.js
@@ -1,16 +1,29 @@
 // <!-- jQuery Slick --> 
   $(document).ready(() => {
-
     $(window).scroll( () => {
       let scroll = $(window).scrollTop();
       let offset = $("#home").offset().top; 
       let height = $("#home").height(); 
-      if (scroll > offset + height - 28) {
+      if (scroll > (offset + height) - 28) {
         $(".navbar-text").css("color","black");
-        // $(".navbar-brand").css("color","black");
+        $(".navbar-brand").css("color","black");
       } else {
         $(".navbar-text").css("color","white");
-        // $(".navbar-brand").css("color","white");
+        $(".navbar-brand").css("color","white");
       }
     });
+
+    // // loader
+    //   var loader = function() {
+    //     setTimeout(function() { 
+    //       if($('#ftco-loader').length > 0) {
+    //         $('#ftco-loader').removeClass('show');
+    //       }
+    //     }, 1);
+    //     };
+    //   loader();
   });
+
+
+
+  
