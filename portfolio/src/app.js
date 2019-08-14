6b952c7e5afb96a1908ac28aab938bb98c7ebacc
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2019-08-14 15:24:35 +0200

    Remove old code and add get requests

--- portfolio/src/app.js
+++ portfolio/src/app.js
@@ -1,24 +1,36 @@
 const express = require("express");
-const app   = express();
-const path = require('path');
+const app = express();
+const path = require("path");
+const bodyParser = require('body-parser');
+
 const port  = process.env.PORT || 4000;
 
-var bodyParser = require('body-parser');
-app.use('/', bodyParser.urlencoded({extended:true}));
-
-// Set src folder as root
 app.use(express.static(__dirname + "/../public"));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
 
-app.get("/", (req, res, next) => {
-  res.sendFile("src/views/index.html");
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html")
 });
 
-app.post("/form", (req, res, next) => {
+app.get("/allBlogs", (req, res) => {
+  res.sendFile(__dirname + "/views/blogs.html");
+});
 
-})
+app.get("/bootstrapProjects", (req, res) => {
+  res.sendFile(__dirname + "/views/bootstrap.html");
+});
+
+app.get("/healthcheck", (req, res) => {
+  res.send("I'm Okay!");
+});
+
 // Listen for HTTP requests on port 3000
 app.listen(port, () => {
-  console.log("listening on %d", port);
+  console.log("Listening on %d", port);
 });
 
 module.exports = app;
