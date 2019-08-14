797d8c7d31c80e39bb42020bafa2503e4ad9f06f
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2019-08-14 15:24:41 +0200

    Update .eslintrc.js

--- portfolio/.eslintrc.js
+++ portfolio/.eslintrc.js
@@ -1,19 +1,11 @@
 module.exports = {
+  root: true,
   env: {
-    browser: true,
-    commonjs: true,
-    es6: true
+    node: true
   },
   extends: [
-    "standard"
+    "eslint:recommended"
   ],
-  globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
-  },
-  parserOptions: {
-    ecmaVersion: 2018
-  },
   rules: {
     "semi": ["error", "always"],
     "quotes": ["error", "double"]
