591b7ceb5494111257c7e86fcd2408b2a6fc787f
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2019-08-19 11:04:05 +0200

    Fix eslint warnings

--- portfolio/src/app.js
+++ portfolio/src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
-const path = require("path");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 const port = process.env.PORT || 4000;
 
