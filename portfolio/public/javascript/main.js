4dc9447baa8bac80577d8d373fdb31ad19fa22dc
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2019-08-19 13:30:26 +0200

    Replace slick to bootstrap carousel

--- portfolio/public/javascript/main.js
+++ portfolio/public/javascript/main.js
@@ -1,25 +1,7 @@
 // <!-- jQuery --> 
   $(document).ready(() => {
-    $(window).scroll( () => {
-      let scroll = $(window).scrollTop();
-      let offset = $("#home").offset().top; 
-      let height = $("#home").height(); 
-      if (scroll > (offset + height) - 28) {
-        $(".navbar-text").css("color","black");
-        $(".navbar-brand").css("color","black");
-      } else {
-        $(".navbar-text").css("color","white");
-        $(".navbar-brand").css("color","white");
-      }
-    });
-
+    $(".carousel").carousel();
     $(".email-decoy").hide();
-
-    $(".slick-container").slick({
-      slidesToShow: 1,
-      centerMode: true,
-      dots: true
-    });
   });
 
 
