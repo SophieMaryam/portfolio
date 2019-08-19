11ee8c7bb26715b077b3fa11cfcb4381fbc835ea
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2019-08-19 09:32:46 +0200

    Update app.js

--- portfolio/src/app.js
+++ portfolio/src/app.js
@@ -1,9 +1,9 @@
-var express = require("express");
-var app = express();
-var path = require("path");
-var bodyParser = require('body-parser');
+const express = require("express");
+const app = express();
+const path = require("path");
+const bodyParser = require('body-parser');
 
-var port = process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 
 app.use(express.static(__dirname + "/../public"));
 app.use(bodyParser.urlencoded({
